b61f589a8b72dd3c86fe9e11d81322c52a62328b
dhaval.trivedi <dhaval.trivedi@volansys.com>
2023-07-14 15:22:02 +0530

    initial commit

---
+++ SrollableTabbarSwift/SrollableTabbarSwift.h
@@ -0,0 +1,18 @@
+//
+//  SrollableTabbarSwift.h
+//  SrollableTabbarSwift
+//
+//  Created by Dhaval Trivedi on 14/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SrollableTabbarSwift.
+FOUNDATION_EXPORT double SrollableTabbarSwiftVersionNumber;
+
+//! Project version string for SrollableTabbarSwift.
+FOUNDATION_EXPORT const unsigned char SrollableTabbarSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SrollableTabbarSwift/PublicHeader.h>
+
+
